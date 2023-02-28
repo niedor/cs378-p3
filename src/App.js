df9f0dbad95794219cdc99b5e28c3b0586502ed3
Dorothy <dorothynie@gmail.com>
2023-02-27 22:39:48 -0600

    best sellers foundation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import Search from "./search.svg";
 import './App.css';
 
+function Button({year, url, setDisplayData}) {
+  async function handleClick(url) {
+    try {
+      const response = await fetch(url)
+      const json = await response.json();
+      setDisplayData(json);
+      console.log(json);
+
+    } catch (e) {
+      console.log("There was an error: " + e);
+      // TO DO: Create an error pop-up in case something doesn't go to plan
+    }
+  }
+  
+  return (
+    <button
+      onClick={() => handleClick(url)}
+    >
+      {year}
+    </button>
+  )
+}
+
+
 function App() {
+  const [displayData, setDisplayData] = useState('');
+  const [hide, setHide] = useState(false);
+  const [searchDate, setSearchDate] = useState('');
+  const [searchDisplayData, setSearchDisplayData] = useState('');
+
+  function handleCollapsible() {
+    setHide(!hide);
+  }
+
+  async function loadSearchDisplayData() {
+    console.log("I got into loadSearch");
+
+    try {
+      // const search_url = `https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=${searchDate}&api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6`;
+      let search_url = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6';
+      const response = await fetch(search_url);
+      const json = await response.json();
+      setSearchDisplayData(json);
+      console.log("Search Display Data: " + json);
+    } catch (e) {
+      console.log("Error: " + e);
+    }
+  }
+
+  const handleSearchDate = (e) => {
+    setSearchDate(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Look! BOOKS</h1>
+
+      <form>
+        <input className='search' type="text" placeholder="Search by date (YYYY-MM-DD)" onChange={handleSearchDate} />
+        <button className='submit' type="submit" onClick={loadSearchDisplayData}>
+          Go!
+        </button>
+      </form>
+
+      <p>Need a starting point? </p>
+      <p>Look through these most recent New York Time's Best Sellers by week:</p>
+      <div>
+        <Button year='Latest' url='https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6' setDisplayData={setDisplayData}/>
+        <Button year='Feb 18' url='https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=2023-02-27&api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6' setDisplayData={setDisplayData}/>
+        <Button year='Feb 11' url='https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=2023-02-26&api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6' setDisplayData={setDisplayData}/>
+        <Button year='Feb 4' url='https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=2023-02-19&api-key=z3irrlRzdl2CBdcwjRSK7dA96whVEOS6' setDisplayData={setDisplayData}/>
+      </div>
+      
+      {searchDisplayData ? (searchDisplayData.results.lists.map((list) => {
+        return (
+          <div>
+            <button className='list-name' onClick={handleCollapsible} key={list.index}>{list.list_name}</button>
+            {hide && list.books.map((book) => (
+              <p className='book-name' key={book.index}>{book.title}, {book.author}</p>
+            ))}
+          </div>
+        );
+      })) : ''}
+      
+      
+      {displayData ? (displayData.results.lists.map((list) => {
+        return (
+          <div>
+            <button className='list-name' onClick={handleCollapsible} key={list.index}>{list.list_name}</button>
+            {hide && list.books.map((book) => (
+              <p className='book-name' key={book.index}>{book.title}, {book.author}</p>
+            ))}
+          </div>
+        );
+      })) 
+        : ''}
+
     </div>
   );
 }
